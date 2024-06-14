217263f91b6d3a955c164266c7ec6630117c70de
BalakeKarbon <blakekarbon@gmail.com>
2024-06-14 17:00:01 +0000

    Forgot to remove old example folders from commit!

--- example/compile-native.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-f2c subf.f
-cobc -x -C main.cbl -K r_
-gcc subf.c main.c -lgmp -lcob -o example
--- example/compile-wasm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-f2c subf.f
-cobc -x -C main.cbl -K r_
-#emcc subf.c main.c -I/root/gmp-6.3.0/ -I/root/gnucobol-3.2/ -I/root/libf2c -L/root/gmp-6.3.0/.libs/ -L/root/gnucobol-3.2/libcob/.libs/ -lgmp -lcob -o example.js
-emcc subf.c main.c -lgmp -lcob -o example.js
