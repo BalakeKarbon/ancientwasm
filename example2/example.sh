09eefc68082404481cc60c453117e24f5d5eb710
BalakeKarbon <blakekarbon@gmail.com>
2024-06-14 02:16:45 +0000

    Fixed 32-64 bit data type issues.

--- example2/example.sh
+++ example2/example.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 f2c f1.f
-f2c f2.f #Currently an issue here where f2c declares a subroutine as an int but it is defined as a void.
+f2c f2.f 
 sed -i 's/int s_copy/void s_copy/g' f2.c
 cobc -x -C main.cbl -K f_ -K ff_ -K emscripten_run_script
-sed -i 's/cob_u64_t/cob_u32_t/g' main.c #How do modify main.cbl so type is 32 bit?
 emcc f1.c f2.c main.c -lgmp -lcob -lf2c -o example.js
+echo "Running example..."
+node example.js
--- example2/main.cbl
+++ example2/main.cbl
@@ -7,18 +7,19 @@
        01 JSCODE PIC A(37) VALUE 'console.log("Hello from Javascript")'.
        01 RETURN2 USAGE IS BINARY-LONG SIGNED.
        01 RS PIC X(26) VALUE HIGH-VALUE.
-       01 RSV USAGE IS BINARY-CHAR UNSIGNED.
-      *01 RETURNP1 REDEFINES RETURN1 USAGE POINTER.
+       01 RSV PIC X(1).
        PROCEDURE DIVISION.
        000-Main. *>Can this be something else?
            DISPLAY "Hello from COBOL"
            CALL 'emscripten_run_script' USING BY CONTENT JSCODE
-001600     MOVE 26 TO RSV
+           MOVE 26 TO RSV
            CALL 'ff_' USING BY REFERENCE RS, BY VALUE RSV,
                RETURNING RETURN2
+      *This function passes the wrong data type into the string length
+      *but it is working for now.         
            DISPLAY RS
            DISPLAY "ff return: " RETURN2
-           PERFORM LOOP1 VARYING INDEX1 FROM 0 BY 1 UNTIL INDEX1 > 2
+           PERFORM LOOP1 VARYING INDEX1 FROM 0 BY 1 UNTIL INDEX1 > 5
            STOP RUN.
        LOOP1.
            CALL 'f_' USING BY CONTENT INDEX1, RETURNING RETURN1
